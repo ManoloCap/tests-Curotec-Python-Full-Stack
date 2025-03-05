633b2ff7079acbada93a9d910b92a6432a0e2be3
Manolo <capilla.manolo@gmail.com>
2025-03-05 00:52:32 -0600

    update: Adding from_attributes to ItemResponse Schema

--- app/api/items.py
+++ app/api/items.py
@@ -1,17 +1,37 @@
 from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy.orm import Session
-from app.db.operations import get_items, create_item
+from app.db.operations import get_items, create_item, get_items_count
 from app.schemas.item import ItemResponse, ItemCreate 
 from app.db.session import get_db
+import math
 
 router = APIRouter()
 
-@router.get("/", response_model=list[ItemResponse])
-async def read_items(skip: int = Query(0, ge=0), limit: int = Query(10, ge=1, le=100), db: Session = Depends(get_db)):
+@router.get("/", response_model=dict)
+async def read_items(
+    page: int = Query(default=1, ge=1, description="Page number as a query parameter"),
+    per_page: int = Query(default=10, ge=1, le=100, description="Number of items per page as a query parameter"),
+    db: Session = Depends(get_db)
+):
+    skip = (page - 1) * per_page
+    limit = per_page
     items = get_items(db, skip=skip, limit=limit)
-    if not items:
-        raise HTTPException(status_code=404, detail="Items not found")
-    return items
+    total_count = get_items_count(db)
+
+    if not items and page != 1:
+        raise HTTPException(status_code=404, detail="Page not found")
+
+    items_response = [ItemResponse.from_orm(item) for item in items]
+
+    total_pages = math.ceil(total_count / per_page)
+
+    return {
+        "items": items_response,
+        "total_pages": total_pages,
+        "current_page": page,
+        "per_page": per_page,
+        "total_count": total_count
+    }
 
 @router.post("/", response_model=ItemResponse, status_code=status.HTTP_201_CREATED)
 async def create_item_endpoint(item_data: ItemCreate, db: Session = Depends(get_db)):
--- app/schemas/item.py
+++ app/schemas/item.py
@@ -9,6 +9,5 @@ class ItemCreate(ItemBase):
 
 class ItemResponse(ItemBase):
     id: int
-
     class Config:
-        orm_mode = True
+        from_attributes = True 
