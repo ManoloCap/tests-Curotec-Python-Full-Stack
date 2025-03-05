efc1d2234817732fc4630ed02d983f2b4ed974ea
Manolo <capilla.manolo@gmail.com>
2025-03-05 16:44:33 -0600

    update: Adding Cors support

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.db.operations import init_db
 from app.api.router import include_app_router
 
@@ -8,6 +9,20 @@ app = FastAPI()
 init_db()
 
 app = include_app_router(app)
+
+
+origins = [
+    "http://localhost:3000",
+]
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 if __name__ == "__main__":
     import uvicorn
