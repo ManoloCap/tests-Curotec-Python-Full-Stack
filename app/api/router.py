e24281b141643cd883ec5835e919c15472f4b9b3
Manolo <capilla.manolo@gmail.com>
2025-03-05 02:38:25 -0600

    update: adding redis router to the app router

--- app/api/router.py
+++ app/api/router.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
-from app.api.items import router
-
+from app.api.items import items_router
+from app.api.redis import redis_router
 def include_app_router(app: FastAPI):
     """ Registers the items router with the FastAPI application """
+    
+    #Items Router
     app.include_router(
-        router,
+        items_router,
         prefix="/items",
         tags=["items"]
+    )
+    
+    #Items Router
+    app.include_router(
+        redis_router,
+        prefix="/redis",
+        tags=["redis"]
     )
     
     return app
